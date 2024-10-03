19c06837b7372246b2efce40355c5b4d2dddfd54
Thetrolxs <moralesharnisch@gmail.com>
2024-10-02 22:47:05 -0300

    feat: added Seeders and Migrations

---
+++ src/Data/DataSeeder.cs
@@ -0,0 +1,86 @@
+
+using Bogus;
+using Catedra1.src.Models;
+
+namespace Catedra1.src.Data
+{
+    public class DataSeeder
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<DataContext>();
+
+                if(!context.Genders.Any())
+                {
+                    context.Genders.AddRange(
+                        new Gender { Type = "masculino"},
+                        new Gender { Type = "femenino"},
+                        new Gender { Type = "otro"},
+                        new Gender { Type = "prefiero no decirlo"}
+                    );
+                }
+
+                context.SaveChanges();
+
+                var generatedRuts = new HashSet<string>();
+
+                var faker = new Faker<User>()
+                .RuleFor(u => u.Rut, f => GenerateUniqueRandomRut(generatedRuts))
+                .RuleFor(u => u.Name, f => f.Person.FullName)
+                .RuleFor(u => u.Email, f => f.Internet.Email())
+                .RuleFor(u => u.GenderId, f => f.Random.Number(1,4))
+                .RuleFor(u => u.Birthday, f => f.Date.Past(30, DateTime.Now.AddYears(-18)));
+
+                var users = faker.Generate(20);
+
+                context.Users.AddRange(users);
+                
+            }
+        }
+    private static string GenerateUniqueRandomRut(HashSet<string> existingRuts)
+    {
+        string rut;
+        do
+        {
+            rut = GenerateRandomRut();
+        } while (existingRuts.Contains(rut));
+
+        existingRuts.Add(rut);
+        return rut;
+    }
+
+    private static string GenerateRandomRut()
+    {
+        Random random = new();
+        int rutNumber = random.Next(1, 99999999); // Número aleatorio de 7 o 8 dígitos
+        int verificator = CalculateRutVerification(rutNumber);
+        string verificatorStr = verificator.ToString();
+        if(verificator == 10){
+            verificatorStr = "k";
+        }
+
+        return $"{rutNumber}-{verificatorStr}";
+    }
+
+    // Método para calcular el dígito verificador de un RUT
+    private static int CalculateRutVerification(int rutNumber)
+    {
+        int[] coefficients = { 2, 3, 4, 5, 6, 7 };
+        int sum = 0;
+        int index = 0;
+
+        while (rutNumber != 0)
+        {
+            sum += rutNumber % 10 * coefficients[index];
+            rutNumber /= 10;
+            index = (index + 1) % 6;
+        }
+
+        int verification = 11 - (sum % 11);
+        return verification == 11 ? 0 : verification;
+    }
+    }
+}
---
+++ src/Data/Migrations/20241003014546_Initial.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Catedra1.src.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Type = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Rut = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Birthday = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    GenderId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Genders_GenderId",
+                        column: x => x.GenderId,
+                        principalTable: "Genders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_GenderId",
+                table: "Users",
+                column: "GenderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Genders");
+        }
+    }
+}
